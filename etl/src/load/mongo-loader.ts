e69933d29bc4699f89faa4520d081cf3ee6058e9
Orekhov Artemy <timorekhov@gmail.com>
2019-09-15 14:24:25 +0200

    bootstrap load

---
+++ etl/src/load/mongo-loader.ts
@@ -0,0 +1,7 @@
+import { Corpus } from '../corpus';
+
+export class MongoLoader {
+    async load(corpus: Corpus) {
+        console.log('Load to mongo');
+    }
+}
--- etl/src/main.ts
+++ etl/src/main.ts
@@ -1,32 +1,57 @@
 import { Command } from "commander";
-import { configs, SourceFormat } from "./etl-configs";
+import { configs, SourceFormat, EtlConfig, Destination } from "./etl-configs";
 import { AnkiTxtExtractor } from "./extract/ankitxt-extractor";
+import { Corpus } from "./corpus";
+import { MongoLoader } from "./load/mongo-loader";
 
 export async function start() {
+    try {
+        const activeConfiguration = loadConfiguration();
+        await etl(activeConfiguration);
+        console.log('Finished.');
+    } catch (error) {
+        console.error(`Error occured.`);
+        console.error(error);
+    }
+}
+
+function loadConfiguration(): EtlConfig {
     const program = new Command();
-
     program.option('-c, --config <value>', 'display help message');
-
     program.parse(process.argv);
 
     const activeConfiguration = configs.find((c) => c.id === program.config);
-    if(activeConfiguration === undefined) {
+    if (activeConfiguration === undefined) {
         throw `configuration not found: ${program.config}`;
     }
 
     console.log(`Running ETL with the following config: ${activeConfiguration.description}`);
+    return activeConfiguration;
+}
 
+async function etl(activeConfiguration: EtlConfig) {
+    const corpus = await extract(activeConfiguration);
+    await load(activeConfiguration, corpus);
+}
+
+async function extract(activeConfiguration: EtlConfig): Promise<Corpus> {
     if (activeConfiguration.sourceFormat === SourceFormat.AnkiTxt) {
         const extractor = new AnkiTxtExtractor();
-        const corpus = await extractor.extract({
+        return await extractor.extract({
             filePath: activeConfiguration.sourceOptions.path,
             fieldNumber: activeConfiguration.sourceOptions.fieldNumber,
             known: activeConfiguration.known,
             NGramLength: 3
         });
-        // load to DB
-    };
-
-    console.log('Finished');
+    }
+    throw `Unknown source format: ${activeConfiguration.sourceFormat}`;
 }
 
+async function load(activeConfiguration: EtlConfig, corpus: Corpus) {
+    if (activeConfiguration.dest === Destination.Mongo) {
+        const loader = new MongoLoader();
+        await loader.load(corpus);
+    } else {
+        throw `Unknown dest format: ${activeConfiguration.dest}`;
+    }
+}
