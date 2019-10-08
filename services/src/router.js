1efe26be663d92168a7eb085c8c48bf1c0e08566
Artemy Orekhov <timorekhov@gmail.com>
2019-10-08 19:46:40 +0200

    cleanup old routes

--- services/src/router.js
+++ services/src/router.js
@@ -8,25 +8,6 @@ router.get('/test', (req, res) => {
     res.status(200).send('Services are bootstrapped!').end();
 });
 
-router.get('/words', (req, res) => {
-    const { lang } = req.query;
-    res.status(200)
-        .json(wordsMock)
-        .end();
-});
-
-router.get('/unigrams', async (req, res) => {
-    const { limit } = req.query;
-    limitN = parseInt(limit);
-    if (!Number.isInteger(limitN)) {
-        limitN = 10;
-    }
-    const unigrams = await data.dataAccess.getUnigrams(limitN);
-    res.status(200)
-        .json(unigrams)
-        .end();
-});
-
 router.get('/ngrams/:lang/:type', async (req, res) => {
     const { limit, known } = req.query;
     const { lang, type } = req.params;
