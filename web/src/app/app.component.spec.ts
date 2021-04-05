53cd0836f8c1c9b8e434c2c0abc7432452f50435
Artemy Orekhov <timorekhov@gmail.com>
2021-04-05 17:27:25 +0200

    update angular to version 11

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         BrowserModule,
